73530981862a357e7eb0baba3cb2f1003d3b56a3
FrostLua <139758993+FrostLua@users.noreply.github.com>
2024-08-05 12:58:39 +0200

    Create CrackedLoader.lua

---
+++ DrownedHub/CrackedLoader.lua
@@ -0,0 +1,42 @@
+local PlaceId = game.PlaceId
+if PlaceId == 4871329703 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DrownedHubTypesoul.lua"))()
+elseif PlaceId == 14071822972 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DrownedHubTypesoul.lua"))()
+elseif PlaceId == 14069122388 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DrownedHubTypesoul.lua"))()
+elseif PlaceId == 14069678431 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DrownedHubTypesoul.lua"))()
+elseif PlaceId == 14069866342 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DrownedHubTypesoul.lua"))()
+elseif PlaceId == 14069956183 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DrownedHubTypesoul.lua"))()
+elseif PlaceId == 3246009126 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/LegacyPiece.lua"))()
+elseif PlaceId == 2633076999 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DemonPiece.lua"))()
+elseif PlaceId == 6897167394 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/DemonPiece.lua"))()
+elseif PlaceId == 4984260245 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/Cursedsea.lua"))()
+elseif PlaceId == 1650291138 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/Demonfall.lua"))()
+elseif PlaceId == 5434161916 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/ShinobiLineage.lua"))()
+elseif PlaceId == 17450551531 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/ShinobiLineage.lua"))()
+elseif PlaceId == 11424731604 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/BrGpo.lua"))()
+elseif PlaceId == 648454481 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/BrGpo.lua"))()
+elseif PlaceId == 6040823996 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/Xl.lua"))()
+elseif PlaceId == 578392296 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/Aba.lua"))()
+elseif PlaceId == 3936365689 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/Clover.lua"))()
+elseif PlaceId == 6035872082 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/Rival.lua"))()
+elseif PlaceId == 6216468795 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/DrownedHub/Weakleg.lua"))()
+end
