2e86f8ad8021b6b729c045ad555399df132befb0
FrostLua <139758993+FrostLua@users.noreply.github.com>
2024-08-06 10:17:17 +0200

    Update MM.lua

--- ElgatoHub/MM.lua
+++ ElgatoHub/MM.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/ElgatoHub/Stuff.lua"))();
+
 local old = loadstring
 local tpc = 0
 getgenv().loadstring = function(a)
