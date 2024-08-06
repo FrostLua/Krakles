2603bf54691876ed237ad3bd8341613033b18050
FrostLua <139758993+FrostLua@users.noreply.github.com>
2024-08-06 10:21:18 +0200

    Create Loader.lua

---
+++ ElgatoHub/Loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 6897167394 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/ElgatoHub/FS.lua"))();
+elseif game.PlaceId == 10260193230 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrostLua/Krakles/main/ElgatoHub/MM.lua"))();
+else
+  warn("Game is not supported!");
+end;
